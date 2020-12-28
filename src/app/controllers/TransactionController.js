e2d36bd3cc91a7888dfad9f6e445256a7530acaa
AnhHoangQuach <hoanganh36.work@gmail.com>
2020-12-29 02:14:33 +0700

    feat: add buy page transaction

--- src/app/controllers/TransactionController.js
+++ src/app/controllers/TransactionController.js
@@ -1,11 +1,15 @@
 class TransactionController {
     index(req, res) {
-        if(req.query.step === 2) {
-            res.render('buy2', {title: 'Mua bước 2', link: 'buy2'})
-        } else if (req.query.step === 3) {
-            res.render('buy3', {title: 'Mua bước 3', link: 'buy3'})
+        if (req.query.step == 2) {
+            res.render('buy2', { title: 'Mua bước 2', link: 'buy2' })
+        } else if (req.query.step == 3) {
+            res.render('buy3', { title: 'Mua bước 3', link: 'buy3' })
+        } else if (req.query.step == 1) {
+            res.render('buy1', { title: 'Mua bước 1', link: 'buy1' })
+        } else if (req.query.step == 4) {
+            res.render('buy4', { title: 'Mua bước 4', link: 'buy4' })
         } else {
-            res.render('buy1', {title: 'Mua bước 1', link: 'buy1'})
+            res.render('transaction', { title: 'Giao dịch', link: 'transaction' })
         }
     }
 
