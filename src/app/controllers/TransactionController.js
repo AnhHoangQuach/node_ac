be5160bee1ea8df4ee8b2e34fe156bf6bcf3d4de
AnhHoangQuach <hoanganh36.work@gmail.com>
2020-12-28 12:02:54 +0700

    fix: change router link

--- src/app/controllers/TransactionController.js
+++ src/app/controllers/TransactionController.js
@@ -1,7 +1,15 @@
 class TransactionController {
     index(req, res) {
-        res.render('transaction', {title: 'Giao dịch', link: 'transaction'})
+        if(req.query.step === 2) {
+            res.render('buy2', {title: 'Mua bước 2', link: 'buy2'})
+        } else if (req.query.step === 3) {
+            res.render('buy3', {title: 'Mua bước 3', link: 'buy3'})
+        } else {
+            res.render('buy1', {title: 'Mua bước 1', link: 'buy1'})
+        }
     }
+
+
 }
 
 module.exports = new TransactionController
