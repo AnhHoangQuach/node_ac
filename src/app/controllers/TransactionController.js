ee90772e71f413f8eac4f3f36a884789a71cd11c
AnhHoangQuach <hoanganh36.work@gmail.com>
2020-12-29 17:55:41 +0700

    feat: add sell page and delete redundant file

--- src/app/controllers/TransactionController.js
+++ src/app/controllers/TransactionController.js
@@ -1,19 +1,35 @@
 class TransactionController {
-    index(req, res) {
-        if (req.query.step == 2) {
-            res.render('buy2', { title: 'Mua bước 2', link: 'buy2' })
-        } else if (req.query.step == 3) {
-            res.render('buy3', { title: 'Mua bước 3', link: 'buy3' })
-        } else if (req.query.step == 1) {
-            res.render('buy1', { title: 'Mua bước 1', link: 'buy1' })
-        } else if (req.query.step == 4) {
-            res.render('buy4', { title: 'Mua bước 4', link: 'buy4' })
-        } else {
-            res.render('transaction', { title: 'Giao dịch', link: 'transaction' })
-        }
+    index(req, res, next ) {
+        res.render('transaction', {title: 'Giao dịch', link: 'transaction'})
     }
 
+    buy(req, res, next) {
+        if (req.query.step == 2) {
+            res.render('buy/buy2', { title: 'Mua bước 2', link: 'buy2' })
+        } else if (req.query.step == 3) {
+            res.render('buy/buy3', { title: 'Mua bước 3', link: 'buy3' })
+        } else if (req.query.step == 1) {
+            res.render('buy/buy1', { title: 'Mua bước 1', link: 'buy1' })
+        } else if (req.query.step == 4) {
+            res.render('buy/buy4', { title: 'Mua bước 4', link: 'buy4' })
+        }
 
+        next();
+    }
+
+    sell(req, res, next) {
+        if (req.query.step == 2) {
+            res.render('sell/sell2', { title: 'Bán bước 2', link: 'sell2' })
+        } else if (req.query.step == 3) {
+            res.render('sell/sell3', { title: 'Bán bước 3', link: 'sell3' })
+        } else if (req.query.step == 1) {
+            res.render('sell/sell1', { title: 'Bán bước 1', link: 'sell1' })
+        } else if (req.query.step == 4) {
+            res.render('sell/sell4', { title: 'Bán bước 4', link: 'sell4' })
+        }
+
+        next();
+    }
 }
 
 module.exports = new TransactionController
