3f802373603a4cb3925b1d8f6fadbef2d99a429b
unknown <Administrator@NS-08.MAY-11>
2021-01-06 19:16:15 +0700

    feat: edit some feature and get new api

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch')
 const url = require('url')
 const api_url = `https://login.acwallet.io/api/v1/rates`
-const api_store_url = `http://login.acwallet.io/api/v1/agency-detail?name=SA88`
+const api_store_url = `https://login.acwallet.io/api/v1/agency-detail?name=MA8888`
 class SiteController {
     async index(req, res, next) {
         const fetch_response = await fetch(api_url)
@@ -10,13 +10,16 @@ class SiteController {
         const fetch_store = await fetch(api_store_url)
         const data_store = await fetch_store.json()
         var path_name = url.parse(api_store_url).query.split('name=')[1]
-        var type
+        var type, avatar
         if ( path_name.includes('SA')) {
             type = 'Super Admin'
+            avatar = 'img/sa-icon.png'
         } else if (path_name.includes('MA')) {
             type = 'Master Agent'
+            avatar = 'img/ma-icon.png'
         } else if (path_name.includes('AG')) {
             type = 'Agent'
+            avatar = 'img/ag-icon.png'
         } else {
             type = ''
         }
@@ -27,13 +30,24 @@ class SiteController {
             rates: data.rates,
             agency: data_store.agency,
             type: type,
+            avatar: avatar,
         })
     }
 
     async showContact(req, res) {
         const fetch_store = await fetch(api_store_url)
         const data_store = await fetch_store.json()
-        res.render('contact', { title: 'Liên hệ', link: 'contact' , agency: data_store.agency})
+
+        var path_name = url.parse(api_store_url).query.split('name=')[1]
+        var avatar
+        if ( path_name.includes('SA')) {
+            avatar = 'img/sa-icon.png'
+        } else if (path_name.includes('MA')) {
+            avatar = 'img/ma-icon.png'
+        } else if (path_name.includes('AG')) {
+            avatar = 'img/ag-icon.png'
+        }
+        res.render('contact', { title: 'Liên hệ', link: 'contact' , agency: data_store.agency, avatar: avatar})
     }
 }
 
--- src/app/controllers/StoreController.js
+++ src/app/controllers/StoreController.js
@@ -4,17 +4,20 @@ class StoreController {
     async index(req, res, next) {
         // const agencyName = req.query.name
         // const api_url = `http://login.acwallet.io/api/v1/agency-detail?name=${agencyName}`
-        const api_url = `http://login.acwallet.io/api/v1/agency-detail?name=SA88`
+        const api_url = `http://login.acwallet.io/api/v1/agency-detail?name=MA8888`
         const fetch_response = await fetch(api_url)
         const data = await fetch_response.json()
         var path_name = url.parse(api_url).query.split('name=')[1]
-        var type
+        var type, avatar
         if ( path_name.includes('SA')) {
             type = 'Super Admin'
+            avatar = 'img/sa-icon.png'
         } else if (path_name.includes('MA')) {
             type = 'Master Agent'
+            avatar = 'img/ma-icon.png'
         } else if (path_name.includes('AG')) {
             type = 'Agent'
+            avatar = 'img/ag-icon.png'
         } else {
             type = ''
         }
@@ -24,6 +27,7 @@ class StoreController {
             agency: data.agency,
             wallet: data.wallets,
             type: type,
+            avatar: avatar,
         })
     }
 }
